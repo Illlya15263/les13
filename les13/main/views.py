f75b00dec76e7ce5fc27a63ebee8458ff98c5a46
Illlya15263 <talkoilli2009@gmail.com>
2023-10-26 12:23:07 +0300

    first commit

--- les13/main/views.py
+++ les13/main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_GET, require_POST
+from .models import get_product_all, check_user
 # Create your views here.
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
